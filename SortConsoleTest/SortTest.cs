69ed513d6a7ad2d34a444ef7f108b07500c83edc
phoenixIS <savchira9@gmail.com>
2017-06-26 20:59:06 +0300

    Add project files.

---
+++ SortConsoleTest/SortTest.cs
@@ -0,0 +1,90 @@
+﻿using SortLogic;
+using System;
+
+namespace SortConsoleTest
+{
+    class TestSort
+    {
+        private const String quickSortName = "Quick Sort";
+        private const String mergeSortName = "Merge Sort";
+        private const int errorIndicator = -1;
+
+        static void Main(string[] args)
+        {
+            int size = GetArraySizeFromConsole();
+            if (size == errorIndicator)
+            {
+                Console.Read();
+                return;
+            }
+            int[] array = GetArrayFromConsole(size);
+
+            Console.WriteLine("The initial array is:");
+            PrintArrayToConsole(array);
+
+            PerformSort(array, Sort.QuickSort, quickSortName);
+            PerformSort(array, Sort.MergeSort, mergeSortName);
+
+            Console.ReadLine();
+        }
+
+        private static void PerformSort(int[] array, Action<int[]> method, String nameOfSort)
+        {
+            int[] arrayForSort = new int[array.Length];
+            Array.Copy(array, arrayForSort, array.Length);
+
+            try
+            {
+                method(arrayForSort);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{nameOfSort} Exception: {e.Message}");
+                return;
+            }
+
+            Console.WriteLine($"The array sorted with {nameOfSort} is:");
+            PrintArrayToConsole(arrayForSort);
+        }
+
+        private static int GetArraySizeFromConsole()
+        {
+            Console.Write("Input the array size: ");
+            String line = Console.ReadLine();
+            if (!int.TryParse(line, out int size) || size < 0)
+            {
+                Console.WriteLine("Wrong size format.");
+                return errorIndicator;
+            }
+            return size;
+        }
+
+        private static int[] GetArrayFromConsole(int size)
+        {
+            Console.WriteLine("Input the array elements.");
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                String str = Console.ReadLine();
+                int value;
+                while (!int.TryParse(str, out value))
+                {
+                    Console.WriteLine("Wrong integer format. Try again.");
+                    str = Console.ReadLine();
+                }
+                array[i] = value;
+            }
+            return array;
+        }
+
+        private static void PrintArrayToConsole(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SortLogic/Sort.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace SortLogic
+{
+    /// <summary>
+    /// Class to sort an integer array using different sorting algorithms
+    /// </summary>
+    public static class Sort
+    {
+        private const String nullAarrayError = "The array cannot be null.";
+        private const String emptyArrayError = "The array cannot be empty.";
+
+        /// <summary>
+        /// Checks the array for being null or empty. If so, throws exceptions.
+        /// Otherwise, invokes the recursive quicksort algorithm.
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        public static void QuickSort(int[] array)
+        {
+            CheckInputArray(array);
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Logic for the recursive quicksort algorithm
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        /// <param name="leftIndex">Start index of the array</param>
+        /// <param name="rightIndex">End index of the array</param>
+        private static void QuickSort(int[] array, int leftIndex, int rightIndex)
+        {
+            int partitionIndex = GetPartitionIndex(array, leftIndex, rightIndex);
+
+            if (leftIndex < partitionIndex - 1)
+            {
+                QuickSort(array, leftIndex, partitionIndex - 1);
+            }
+            if (rightIndex > partitionIndex)
+            {
+                QuickSort(array, partitionIndex, rightIndex);
+            }
+        }
+
+        /// <summary>
+        /// Divides the array into two parts so that the first part contains elements
+        /// less than pivot, whereas the second part contains elements greater than pivot
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        /// <param name="leftIndex">Index to start from</param>
+        /// <param name="rightIndex">Index to end with</param>
+        /// <returns>Partition index</returns>
+        private static int GetPartitionIndex(int[] array, int leftIndex, int rightIndex)
+        {
+            int pivot = array[leftIndex];
+
+            while (leftIndex <= rightIndex)
+            {
+                while (array[leftIndex] < pivot)
+                {
+                    leftIndex++;
+                }
+                while (array[rightIndex] > pivot)
+                {
+                    rightIndex--;
+                }
+                if (leftIndex <= rightIndex)
+                {
+                    Swap(ref array[leftIndex], ref array[rightIndex]);
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+            return leftIndex;
+        }
+
+        private static void Swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+
+        /// <summary>
+        /// Checks the array for being null or empty. If so, throws exceptions.
+        /// Otherwise, invokes the recursive mergesort algorithm.
+        /// </summary>
+        /// <param name="array"></param>
+        public static void MergeSort(int[] array)
+        {
+            CheckInputArray(array);
+            MergeSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Logic for the recursive mergesort algorithm
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        /// <param name="leftIndex">Start index of the array</param>
+        /// <param name="rightIndex">End index of the array</param>
+        public static void MergeSort(int[] array, int leftIndex, int rightIndex)
+        {
+            if (leftIndex < rightIndex)
+            {
+                int middleIndex = (leftIndex / 2) + (rightIndex / 2);
+
+                MergeSort(array, leftIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, rightIndex);
+
+                Merge(array, leftIndex, middleIndex, rightIndex);
+            }
+        }
+
+        /// <summary>
+        /// Logic for merging two sorted parts
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        /// <param name="leftIndex">Start index of the array</param>
+        /// <param name="middleIndex">Boundary index of two sorted parts</param>
+        /// <param name="rightIndex">End index of the array</param>
+        private static void Merge(int[] array, int leftIndex, int middleIndex, int rightIndex)
+        {
+            int left = leftIndex;
+            int right = middleIndex + 1;
+            int[] tmp = new int[(rightIndex - leftIndex) + 1];
+            int tmpIndex = 0;
+
+            while ((left <= middleIndex) && (right <= rightIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tmp[tmpIndex] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tmp[tmpIndex] = array[right];
+                    right++;
+                }
+                tmpIndex++;
+            }
+
+            while (left <= middleIndex)
+            {
+                tmp[tmpIndex] = array[left];
+                left++;
+                tmpIndex++;
+            }
+
+            while (right <= rightIndex)
+            {
+                tmp[tmpIndex] = array[right];
+                right++;
+                tmpIndex++;
+            }
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                array[leftIndex + i] = tmp[i];
+            }
+        }
+
+        private static void CheckInputArray(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nullAarrayError);
+            }
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(emptyArrayError);
+            }
+        }
+    }
+}
